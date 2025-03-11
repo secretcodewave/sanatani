8782a0a354e9b57df55a3d2da31afca09e2d3bbb
Mehul Singh <mehulsingh076@gmail.com>
2025-03-11 19:34:06 +0530

    fix

--- src/app/api/auth/register/route.tsx
+++ src/app/api/auth/register/route.tsx
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server"
 import { z } from "zod"
-import { hash } from "bcrypt
-"
+import { hash } from "bcrypt"
 import { db } from "../../../../../configs/db"
 import { users } from "../../../../../configs/schema"
 
